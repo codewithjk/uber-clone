8bad137b7e16f1867ba3d75e765ec8edb029b1ea
jeevan <jeevankumar8943@gmail.com>
2025-02-17 16:02:55 +0530

    types for images added and created one first welcome page.

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,17 +1,40 @@
-import { router } from "expo-router";
-import { Text, TouchableOpacity} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from 'expo-router';
+import { useRef, useState } from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Swiper from 'react-native-swiper';
+import { onboarding } from 'constants/index';
 
-const Welcome = () =>{
-    return (
-        <SafeAreaView className="flex h-full items-center justify-between bg-white ">
-            <TouchableOpacity onPress={() => {
-                router.replace("/(auth)/sign-up")
-            }} className="w-full flex justify-end items-end p-5">
-                <Text className="text-black text-md font-JakartaBold">Skip</Text>
-            </TouchableOpacity>
-        </SafeAreaView>
-    )
-}
+const Welcome = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between bg-white ">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace('/(auth)/sign-up');
+        }}
+        className="flex w-full items-end justify-end p-5">
+        <Text className="text-md font-JakartaBold text-black">Skip</Text>
+      </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={<View className="mx-1 h-[4px] w-[32px] rounded-full bg-[#E2E8F0]" />}
+        activeDot={<View className="mx-1 h-[4px] w-[32px] rounded-full bg-[#0286FF]" />}
+        onIndexChanged={(index) => setActiveIndex(index)}>
+        {onboarding.map((item: any) => (
+            <View key={item.id} className='flex items-center justify-center p-5'>
+                <Image source={item.image} className='w-full h-[300px]' resizeMode='contain' />
+                <View className='flex flex-row items-center justify-center w-full mt-10'>
+                    <Text className='text-black text-3xl font-bold mx-10 text-center'>{item.title}</Text>
+                </View>
+            <Text className='text-md font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3'>{item.description}</Text>
+          </View>
+        ))}
+      </Swiper>
+    </SafeAreaView>
+  );
+};
 
 export default Welcome;
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
-import { Redirect } from "expo-router";
+import { Redirect } from 'expo-router';
 import '../global.css';
-const Home = () =>{
-    return (<Redirect href="/(auth)/welcome"/>)
-}
+const Home = () => {
+  return <Redirect href="/(auth)/welcome" />;
+};
 
 export default Home;
